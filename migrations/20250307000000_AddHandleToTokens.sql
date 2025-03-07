ae92c6bb1775cf8ac5fb9f3635e420fa04a24ecd
Corey Alexander <coreyja@gmail.com>
2025-03-07 12:38:19 -0500

    Reset old migration

--- migrations/20250307000000_AddHandleToTokens.sql
+++ migrations/20250307000000_AddHandleToTokens.sql
@@ -1,5 +1,4 @@
 -- Add handle column to oauth_tokens table
 ALTER TABLE oauth_tokens ADD COLUMN handle TEXT;
-
 -- Add index for faster lookup by handle
 CREATE INDEX oauth_tokens_handle_idx ON oauth_tokens(handle);
