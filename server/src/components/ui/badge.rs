efa8cf2392dbc3038ca8a1c845c04755108bd652
Corey Alexander <coreyja@gmail.com>
2025-04-20 09:24:50 -0400

    Fix saving of handle to the DB

--- server/src/components/ui/badge.rs
+++ server/src/components/ui/badge.rs
@@ -70,33 +70,21 @@ impl Render for Badge {
     }
 }
 
-pub struct BetaBadge {
-    pub extra_classes: Option<String>,
-}
+pub struct BetaBadge {}
 
 impl BetaBadge {
     pub fn new() -> Self {
-        Self {
-            extra_classes: None,
-        }
-    }
-
-    pub fn with_classes(mut self, classes: &str) -> Self {
-        self.extra_classes = Some(classes.to_string());
-        self
+        Self {}
     }
 }
 
 impl Render for BetaBadge {
     fn render(&self) -> Markup {
-        let extra_classes = self.extra_classes.as_deref().unwrap_or("");
-
         html! {
             div class="mb-3" {
-                span class=(format!(
-                    "inline-flex items-center px-4 py-1.5 rounded-full text-sm sm:text-base font-bold bg-gradient-to-r from-blue-400 to-purple-400 text-white shadow-sm transform hover:scale-105 transition-transform duration-200 {}",
-                    extra_classes
-                )) {
+                span class=
+                    "inline-flex items-center px-4 py-1.5 rounded-full text-sm sm:text-base font-bold bg-gradient-to-r from-blue-400 to-purple-400 text-white shadow-sm transform hover:scale-105 transition-transform duration-200"
+                 {
                     "BETA"
                 }
             }
